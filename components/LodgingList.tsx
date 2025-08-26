7e118cd6704efe511052b92481e4d1aa6b90edfe
moreeldavid-sudo <moreel.david@gmail.com>
2025-08-26 18:09:32 +0200

    Update LodgingList.tsx

--- components/LodgingList.tsx
+++ components/LodgingList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../lib/firebase";
 import LodgingCard from "./LodgingCard";
 
@@ -18,20 +18,12 @@ export default function LodgingList() {
   const [lodgings, setLodgings] = useState<Lodging[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "lodgings"));
-        const data: Lodging[] = [];
-        querySnapshot.forEach((doc) => {
-          data.push({ id: doc.id, ...doc.data() } as Lodging);
-        });
-        setLodgings(data);
-      } catch (error) {
-        console.error("Erreur lors du chargement des logements :", error);
-      }
-    };
-
-    fetchData();
+    const unsub = onSnapshot(collection(db, "lodgings"), (snap) => {
+      const data: Lodging[] = [];
+      snap.forEach((d) => data.push({ id: d.id, ...(d.data() as any) }));
+      setLodgings(data);
+    });
+    return () => unsub();
   }, []);
 
   return (
