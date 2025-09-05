de000b0c987ba8757ff393055f65fbfb12343cfb
moreeldavid-sudo <moreel.david@gmail.com>
2025-09-05 14:57:06 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Réservations Hébergements",
@@ -15,8 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        <header className="p-4">
+          <Image
+            src="/Domaine.jpg"
+            alt="Logo Domaine"
+            width={150}
+            height={80}
+            priority
+          />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
-
