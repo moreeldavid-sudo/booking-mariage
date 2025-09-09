d8da105e917ad065e28101316b16fc9dfade7e68
moreeldavid-sudo <moreel.david@gmail.com>
2025-09-09 16:26:07 +0200

    Update LodgingList.tsx

--- components/LodgingList.tsx
+++ components/LodgingList.tsx
@@ -4,7 +4,7 @@ export default function LodgingList({ lodgings }: { lodgings: any[] }) {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {lodgings.map((l) => (
-        <LodgingCard key={l.id} {...l} />
+        <LodgingCardMin key={l.id} />
       ))}
     </div>
   );
