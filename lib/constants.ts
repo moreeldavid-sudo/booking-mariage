552793bd0bce1fce664e25aaf1ef1f66874eb124
moreeldavid-sudo <moreel.david@gmail.com>
2025-09-10 16:19:57 +0200

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const PRICE_PER_TIPI_TOTAL = 200; // CHF pour tout le séjour
+export const STAY_LABEL = "pour les 3 nuits (26–28 juin 2026)";
+export const TWINT_PHONE_E164 = "+41789028758"; // ton numéro
